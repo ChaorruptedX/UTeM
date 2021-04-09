89f8007b542443a8388210eab3e8c491cde50015
ChaorruptedX <ChaorruptedX@gmail.com>
2021-04-09 22:54:35 +0800

    Reinitialize

---
+++ Samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+	private String name;
+	
+		// Getter
+		public String getName() {
+			return name;
+		}
+
+		// Setter
+		public void setName(String newName) {
+			this.name = newName;
+		}
+}
